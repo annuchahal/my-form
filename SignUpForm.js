92352a7b7d44a10a386bdfa9c433e18404d7ee7a
annuchahal <108255216+annuchahal@users.noreply.github.com>
2022-06-27 17:21:15 +0530

    Add files via upload

---
+++ SignUpForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Buttonse from '../component/Buttonse';
+
+function SignUpForm() {
+  return (
+    <>
+      <form>
+        <div className="lower">
+          <h1>Welcome to our Organization</h1>
+          <Buttonse />
+        </div>
+      </form>
+    </>
+  );
+}
+export default SignUpForm;
