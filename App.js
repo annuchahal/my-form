f52d50915d6103e5aac50690e78e433757d84988
annuchahal <108255216+annuchahal@users.noreply.github.com>
2022-06-27 16:00:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignUpForm from './Screens/SignUpForm';
+
+import LogInForm from './Screens/LogInForm';
+
+function App() {
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default App;
